416a27e90953dce74581a6e56072a3b5cc0c1749
Marcos Alexandr Nogueira Rocha <alxnrocha@users.noreply.github.com>
2026-08-21 14:13:26 +0200

    style: substantially increase vertical breathing room around dividers and sections

--- src/components/property/BookingPolicyTimeline.tsx
+++ src/components/property/BookingPolicyTimeline.tsx
@@ -34,7 +34,7 @@ export function BookingPolicyTimeline({ cancellationDays }: BookingPolicyTimelin
   ];
 
   return (
-    <div className="pt-8 pb-4 space-y-6 px-1 sm:px-0">
+    <div className="pt-12 pb-8 space-y-7 px-1 sm:px-0">
       <h3 className="text-xl font-extrabold text-white tracking-tight">
         Booking policy
       </h3>
